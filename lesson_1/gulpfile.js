9fc750dd854600abad6dacb6fbd0e260bc16ab79
Elvina Ziyadinova <e.ziyadinova@lifard.com>
2017-09-07 18:41:47 +0300

    Простейшая сборка

---
+++ lesson_1/gulpfile.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+// gulp.task('hello', function() {
+//   console.log('Hello Zell');
+// });
+
+
+/* Sass / CSS */
+gulp.task('sass', function(){
+  gulp.src('./assets/stylesheets/application.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(autoprefixer({
+    	browsers: ['last 2 versions', 'ie 11', 'android 4']
+    })) // Using gulp-autoprefixer
+    .pipe(gulp.dest('dist/css'))
+});
+
+
+/* JavaScript */
+gulp.task('scripts', function(){
+	return gulp.src([
+		'https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js',
+		'./assets/javascripts/bootstrap.min.js',
+		'./assets/javascripts/script.js'
+	])
+		.pipe(babel({
+    	presets: ['env']
+    }))
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+/* Global main tasks */
+// Development mode without watching
+gulp.task('default', ['scripts', 'sass']);
