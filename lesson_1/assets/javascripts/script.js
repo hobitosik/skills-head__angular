cb9984f03d24bd1354b9df747cbbbfb1bcc92f10
Elvina Ziyadinova <e.ziyadinova@lifard.com>
2017-09-11 16:52:22 +0300

    Добавлена функция вывода рандомного пользователя используя promise

--- lesson_1/assets/javascripts/script.js
+++ lesson_1/assets/javascripts/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 jQuery(document).ready(function($){
 
 	function randomUser(){
@@ -10,27 +12,85 @@ jQuery(document).ready(function($){
 			var userpic = data.results[0].picture.medium;
 			var usermail = data.results[0].email;
 			var userphone = data.results[0].phone;
-			var year = data.results[0].dob;
-			year = + year.slice(0, 4);
+			var year = + data.results[0].dob.slice(0, 4);
 
 			if (year < 1975){
-				console.log('change');
+				console.log('ta damm');
 				$('#username').text(username);
 				$('#usermail').text(usermail);
 				$('#userphone').text(userphone);
 				$('#userpic').attr('src', userpic);
 				$('.usercard').show();
-			} else {
+			} else {				
+				console.log('wait...');
 				randomUser();
 			}
 		});
 
 	}
 
+	function httpGet(url) {
+
+		return new Promise(function(resolve, reject) {
+
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', url, true);
+
+			xhr.onload = function() {
+				if (this.status == 200) {
+					resolve(this.response);
+				} else {
+					var error = new Error(this.statusText);
+					error.code = this.status;
+					reject(error);
+				}
+			};
+
+			xhr.onerror = function() {
+				reject(new Error("Network Error"));
+			};
+
+			xhr.send();
+		});
+
+	}
+
+	function randomUserPromise(){
+		httpGet('https://randomuser.me/api/')
+			.then(response => {
+				console.log(response);
+				let user = JSON.parse(response);
+
+				let username = user.results[0].name.first + ' ' + user.results[0].name.last;
+				let userpic = user.results[0].picture.medium;
+				let usermail = user.results[0].email;
+				let userphone = user.results[0].phone;
+				let year = + user.results[0].dob.slice(0, 4);
+
+				if (year < 1975){
+					console.log('ta damm');
+					$('#username').text(username);
+					$('#usermail').text(usermail);
+					$('#userphone').text(userphone);
+					$('#userpic').attr('src', userpic);
+					$('.usercard').show();
+				} 
+				else {
+					console.log('wait...');
+					randomUserPromise();
+				}
+			})
+			.catch(error => {
+		    alert(error); // Error: Not Found
+		  });
+	}
+
 	$('#RandomUser').bind('click', function(){
 
-		randomUser();
+		// randomUser();
+		randomUserPromise();
 
 	});
+
 
 });
