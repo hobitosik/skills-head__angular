1945e7c0b8d417b80d44286a69c25c43c9efd099
Elvina Ziyadinova <e.ziyadinova@lifard.com>
2017-09-17 10:00:34 +0300

    Тренеруемся с angular-cli

--- lesson_2/angular-app/src/app/app.module.ts
+++ lesson_2/angular-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { FeedbackComponentComponent } from './feedback-component/feedback-component.component';
+import { ButtonComponentComponent } from './button-component/button-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    FeedbackComponentComponent,
+    ButtonComponentComponent
   ],
   imports: [
     BrowserModule
