d6537569c9bb0f88232a689159d9cac1f67fe15e
Elvina Ziyadinova <e.ziyadinova@lifard.com>
2017-09-21 15:33:02 +0300

    почищены комменты

--- lesson_1/assets/javascripts/script.js
+++ lesson_1/assets/javascripts/script.js
@@ -27,76 +27,50 @@ jQuery(document).ready(function($){
 
 	function httpGet(url) {
 
-		return new Promise(function(resolve, reject) {
+		// fetch
+		return fetch(url)
+			.then(response => {
+				return response.json()
+					.then(data => {
+						return data.results[0];
+		      })
 
-			// xhr
-			// var xhr = new XMLHttpRequest();
-			// xhr.open('GET', url, true);
-
-			// xhr.onload = function() {
-			// 	if (this.status == 200) {
-			// 		resolve(this.response);
-			// 	} else {
-			// 		var error = new Error(this.statusText);
-			// 		error.code = this.status;
-			// 		reject(error);
-			// 	}
-			// };
-
-			// xhr.onerror = function() {
-			// 	reject(new Error("Network Error"));
-			// };
-
-			// xhr.send();
-
-			// fetch
-			fetch(url)
-				.then(response => {
-					if (response.status == 200) {
-							// тут ошибка?
-						response.json().then(data => {
-							resolve(data);
-			      })
-					} else {
-						var error = new Error(response.statusText);
-						error.code = response.status;
-						reject(error);
-					}
-
-		  	})
-				.catch(error => {
-					console.log('Fetch Error :-S', error);
-					reject(new Error("Network Error"));
-				});
-
-		});
+	  	})
+			.catch(error => {
+				console.log('Fetch Error :-S', error);
+				reject(new Error("Network Error"));
+			});
 
 	}
 
 	function randomUserPromise(){
-		httpGet('https://randomuser.me/api/')
-			.then(response => {
-				console.log(response);
-				let user = JSON.parse(response);
 
-				let username = user.results[0].name.first + ' ' + user.results[0].name.last;
-				let userpic = user.results[0].picture.medium;
-				let usermail = user.results[0].email;
-				let userphone = user.results[0].phone;
-				let year = + user.results[0].dob.slice(0, 4);
+		return new Promise((resolve) => {
 
-				if (year < 1975){
-					console.log('ta damm');
-					usercard(username, userpic, usermail, userphone);
-				} 
-				else {
-					console.log('wait...');
-					randomUserPromise();
-				}
-			})
-			.catch(error => {
-		    alert(error); // Error: Not Found
-		  });
+			httpGet('https://randomuser.me/api/')
+				.then(user  => {
+					console.log(user);
+
+					let username = user.name.first + ' ' + user.name.last;
+					let userpic = user.picture.medium;
+					let usermail = user.email;
+					let userphone = user.phone;
+					let year = + user.dob.slice(0, 4);
+
+					if (year < 1975){
+						console.log('ta damm');
+						usercard(username, userpic, usermail, userphone);
+					} 
+					else {
+						console.log('wait...');
+						randomUserPromise();
+					}
+				})
+				.catch(error => {
+			    alert(error); // Error: Not Found
+			  });
+
+		})
 
 		  // сделать: промис в запросе передавать аргумент (сид)
 	}
